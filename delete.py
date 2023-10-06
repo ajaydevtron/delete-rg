4da8bdeb85c3636131083a60fcdeb2d5ebd7da93
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2023-10-06 19:16:28 +0530

    modified error printing

--- delete.py
+++ delete.py
@@ -3,13 +3,20 @@ import subprocess as sp
 from datetime import *
 import os ,sys
 import yaml
-username=sys.argv[1]
-password=sys.argv[2]
-tenantid=sys.argv[3]
+# username=sys.argv[1]
+# password=sys.argv[2]
+# tenantid=sys.argv[3]
+
 
 lock_list=[]
 def lock_list_fun():
-    sp.getoutput('az lock list -o yaml >locklist.yaml')
+    # test=sp.run('az lock list -o yaml >locklist.yaml')
+    command="az lock list -o yaml >locklist.yaml"
+    result = sp.run(command, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+    exit_code = result.returncode
+    error=result.stderr
+    if exit_code == "0" :
+        print(error)
     with open('locklist.yaml', 'r') as file:
         temp_list = yaml.safe_load(file)
     for i in temp_list:
@@ -34,25 +41,30 @@ def delete():
             if rg_name in lock_list:
                 print(rg_name,"======>  This resource is locked so it canot be deleted.")
             else:
-                exit_status =os.system(f'az group delete -n {rg_name} -y')
-                print(rg_name,"=====>This will be deleted")
-
-                if(exit_status==0):
-                    print("deleted RG"+" --> "+ rg_name)
+                command=f'az group delete -n {rg_name} -y'
+                result = sp.run(command, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+                exit_code = result.returncode
+                error=result.stderr
+                if exit_code != "0" :
+                    print(f"The resourceGroup {rg_name} is not deleted having error-: ")
+                    print(error)
+                    print("\n")
                 else:
-                    print("Command fail to execute with exit status -> %d" % exit_status)
+                    print("Deleted RG=====>" + rg_name)
         else:
             print(f"{rg_name} resource group will be delete in {date_taged} after {(date_taged - today_date).days} days")
 
 
-def auth():
-    authoutput=sp.getstatusoutput("az login --service-principal -u {} -p {} --tenant {}".format(username,password,tenantid))
-    return authoutput
-authout=auth()
+# def auth():
+#     authoutput=sp.getstatusoutput("az login --service-principal -u {} -p {} --tenant {}".format(username,password,tenantid))
+#     return authoutput
+# authout=auth()
 
-if(authout[0]==0):
-    print("Authentication is successed")
-    delete()
-else:
-    print("Authentication failed",authout[1])
+# if(authout[0]==0):
+#     print("Authentication is successed")
+#     delete()
+# else:
+#     print("Authentication failed",authout[1])
 
+
+delete()
